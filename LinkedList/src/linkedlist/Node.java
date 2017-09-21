16f9b2eb8291c320850eb6f9812585c78b39568e
reggs <reggs@LAPTOP-N9Q252NT>
2017-09-20 23:09:46 -0400

    Created the Node class

---
+++ LinkedList/src/linkedlist/Node.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+/**
+ *
+ * @author reggs
+ */
+public class Node {
+    int val;
+    Node next;
+
+    Node (){
+        val = 0;
+        next = null;
+    }
+
+    Node(int val){
+        this.val = val;
+        next = null;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
